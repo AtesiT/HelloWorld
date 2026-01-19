b16d7c8267dfb6dfaf564e92d839e7eb918489a9
AtesiT <atesit@mail.ru>
2026-01-19 18:44:24 +0300

    ADD: make circle and swift icon on the ViewController

--- HelloWorld/SwiftLogoView.swift
+++ HelloWorld/SwiftLogoView.swift
@@ -1,15 +1,26 @@
 import SwiftUI
 
 struct SwiftLogoView: View {
+    let color: Color
+    
     var body: some View {
-        Circle()
-            .foregroundStyle(.green)
-            .frame(width: 250, height: 250)
-            .overlay(Circle().stroke(Color.white, lineWidth: 2))
-            .shadow(radius: 10)
+        ZStack {
+            Circle()
+                .foregroundStyle(color)
+                .frame(width: 250, height: 250)
+                .overlay(Circle().stroke(Color.white, lineWidth: 2))
+                .shadow(radius: 10)
+            Image(systemName: "swift")
+                //  Image - по умолчанию инициализирует свои размеры, чтобы поставить свой размер
+                .resizable()
+                .frame(width: 150, height: 150)
+                .foregroundStyle(.white)
+                // Делаем значок стрижа посередине
+                .offset(x: -10, y: -10)
+        }
     }
 }
 
 #Preview {
-    SwiftLogoView()
+    SwiftLogoView(color: .cyan)
 }
